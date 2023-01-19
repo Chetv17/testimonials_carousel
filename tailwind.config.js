31410397bc0bb11fec02c22fb83bc921c634ce80
Chester Vincent <chetvincent@chesters-mbp-2.mynetworksettings.com>
2023-01-19 15:21:32 -0500

    carousel completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-
-
-
-
-
-    
-}
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+          colors: {
+            primary: {
+            'teal-800': '#07484A'
+            ,
+          },
+        }
+      }
+    }
